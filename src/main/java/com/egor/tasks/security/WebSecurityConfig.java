7cd6006d441ffe840a38abc748a4ca75aa1c24fc
Egor Gavrilov <gavrilovegor519@gmail.com>
2023-12-07 15:43:40 +0300

    Fix some error.

---
+++ src/main/java/com/egor/tasks/security/WebSecurityConfig.java
@@ -0,0 +1,15 @@
+package com.egor.tasks.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
