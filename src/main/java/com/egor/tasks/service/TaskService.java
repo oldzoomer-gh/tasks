4d1d64fc0df83ebac8001a2c232d7d5f4ac403b9
Egor Gavrilov <gavrilovegor519@gmail.com>
2023-12-09 12:18:10 +0300

    Prepare to create implementations of some services.

--- src/main/java/com/egor/tasks/service/TaskService.java
+++ src/main/java/com/egor/tasks/service/TaskService.java
@@ -1,13 +1,18 @@
 package com.egor.tasks.service;
 
+import com.egor.tasks.constant.TaskPriority;
+import com.egor.tasks.constant.TaskStatus;
 import com.egor.tasks.dto.TaskDto;
+import com.egor.tasks.dto.TaskNameAndDescriptionDto;
 import com.egor.tasks.exception.UserNotFound;
 import org.springframework.data.domain.Page;
 
 public interface TaskService {
     void create(TaskDto task);
     void delete(Long id);
-    void edit(Long id, TaskDto changes);
+    void editStatus(Long id, TaskStatus status);
+    void editPriority(Long id, TaskPriority priority);
+    void editNameAndDescription(Long id, TaskNameAndDescriptionDto taskNameAndDescription);
     TaskDto getTask(Long id);
     Page<TaskDto> getMultipleTasksForCurrentUser();
     Page<TaskDto> getMultipleTasksForAnotherUser(String email) throws UserNotFound;
