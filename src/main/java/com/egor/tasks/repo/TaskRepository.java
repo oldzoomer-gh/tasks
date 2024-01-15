d9eda9365376925f384f2365d73f845afd8d700f
Egor Gavrilov <gavrilovegor519@gmail.com>
2024-01-15 23:23:54 +0300

    Delete unnecessary code.

--- src/main/java/com/egor/tasks/repo/TaskRepository.java
+++ src/main/java/com/egor/tasks/repo/TaskRepository.java
@@ -5,16 +5,12 @@ import com.egor.tasks.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface TaskRepository extends
-        PagingAndSortingRepository<Task, Long>,
         JpaRepository<Task, Long> {
     // findById is default
 
     Page<Task> findAllByAuthor(User author,
                                Pageable pageable);
 
-    Page<Task> findAllByAssigned(User assigned,
-                               Pageable pageable);
 }
--- src/main/java/com/egor/tasks/repo/UserRepository.java
+++ src/main/java/com/egor/tasks/repo/UserRepository.java
@@ -2,12 +2,10 @@ package com.egor.tasks.repo;
 
 import com.egor.tasks.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.Optional;
 
 public interface UserRepository extends
-        PagingAndSortingRepository<User, Long>,
         JpaRepository<User, Long> {
     // findById is default
 
