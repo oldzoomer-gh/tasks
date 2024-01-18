16eeffe47e6108c66463ceffc21e8995ae7f4eb2
Egor Gavrilov <gavrilovegor519@gmail.com>
2024-01-19 00:21:27 +0300

    Revert "Move many classes to another package."
    This reverts commit 5cd76289e0e2487feff1b9a7c8de15a5d0aa6749.

---
+++ src/main/java/com/egor/tasks/controller/dto/input/LoginAndRegistrationDto.java
@@ -0,0 +1,20 @@
+package com.egor.tasks.controller.dto.input;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class LoginAndRegistrationDto {
+    @Size(max = 50)
+    @NotEmpty
+    @Email
+    private String email;
+
+    @Size(max = 32)
+    @NotEmpty
+    private String password;
+}
