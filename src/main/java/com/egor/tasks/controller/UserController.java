037b8b03aafd910bb9a4af4753a623668324386d
Egor Gavrilov <gavrilovegor519@gmail.com>
2024-02-27 00:17:18 +0300

    Add initial data for Swagger definition for UserController.

--- src/main/java/com/egor/tasks/controller/UserController.java
+++ src/main/java/com/egor/tasks/controller/UserController.java
@@ -5,6 +5,9 @@ import com.egor.tasks.exception.DuplicateUser;
 import com.egor.tasks.exception.IncorrectPassword;
 import com.egor.tasks.exception.UserNotFound;
 import com.egor.tasks.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,12 +21,29 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/login")
-    public String login(@RequestBody LoginAndRegistrationDto loginDTO) throws UserNotFound, IncorrectPassword {
+    @Operation(summary = "Get JWT token by login and password",
+                responses = {
+                        @ApiResponse(description = "JWT token for user",
+                                useReturnTypeSchema = true),
+                        @ApiResponse(responseCode = "403",
+                                description = "User not found, or incorrect password")
+                })
+    public String login(@Parameter(description = "Login data", required = true)
+                            @RequestBody LoginAndRegistrationDto loginDTO)
+                                throws UserNotFound, IncorrectPassword {
         return userService.login(loginDTO);
     }
 
     @PostMapping("/reg")
-    public void reg(@RequestBody LoginAndRegistrationDto userDTO) throws DuplicateUser {
+    @Operation(summary = "Register the user for using this service",
+                responses = {
+                        @ApiResponse(description = "User is registered",
+                                useReturnTypeSchema = true),
+                        @ApiResponse(responseCode = "409",
+                                description = "Duplicate registration data")
+                })
+    public void reg(@Parameter(description = "Registration data", required = true)
+                        @RequestBody LoginAndRegistrationDto userDTO) throws DuplicateUser {
         userService.reg(userDTO);
     }
 
