ee2096c7fa488304b27fc82ab295309420204cb6
Egor Gavrilov <gavrilovegor519@gmail.com>
2024-03-09 20:28:41 +0300

    Adding JavaDoc.

--- src/main/java/com/egor/tasks/service/UserService.java
+++ src/main/java/com/egor/tasks/service/UserService.java
@@ -3,7 +3,21 @@ package com.egor.tasks.service;
 import com.egor.tasks.dto.input.LoginAndRegistrationDto;
 import com.egor.tasks.dto.output.TokenDto;
 
+/**
+ * User management service layer.
+ * @author Egor Gavrilov (gavrilovegor519@gmail.com)
+ */
 public interface UserService {
-    TokenDto login(LoginAndRegistrationDto loginData);
-    void reg(LoginAndRegistrationDto userData);
+    /**
+     * Login user with email and password. Generates JWT Bearer token.
+     * @param loginAndRegistrationDto Login data.
+     * @return JWT Bearer token.
+     */
+    TokenDto login(LoginAndRegistrationDto loginAndRegistrationDto);
+
+    /**
+     * Register user.
+     * @param loginAndRegistrationDto Registration data.
+     */
+    void reg(LoginAndRegistrationDto loginAndRegistrationDto);
 }
