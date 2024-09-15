f75d125b185f59acf98989b644f028ea3ac7e8be
Egor Gavrilov <gavrilovegor519@gmail.com>
2024-09-15 13:15:41 +0300

    Fix NonNull annotation warning.

--- src/main/java/ru/gavrilovegor519/tasks/security/JwtAuthFilter.java
+++ src/main/java/ru/gavrilovegor519/tasks/security/JwtAuthFilter.java
@@ -1,19 +1,20 @@
 package ru.gavrilovegor519.tasks.security;
 
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import java.io.IOException;
+
+import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import java.io.IOException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Component
